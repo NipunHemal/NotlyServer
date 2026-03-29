0ce4e344682c309748b13e491d1c206ed615fb40
NipunHemal <nipunthemal@gmail.com>
2026-03-29 23:55:55 +0530

    add NotlyServerApplicationTests class for application context testing

---
+++ src/test/java/lk/hemal/notly/NotlyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.hemal.notly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotlyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
