0c714c0b66add673ede8fbe304c3ca3a7656475a
NipunHemal <nipunthemal@gmail.com>
2026-03-29 23:54:48 +0530

    add NotlyServerApplication class for Spring Boot application entry point

---
+++ src/main/java/lk/hemal/notly/NotlyServerApplication.java
@@ -0,0 +1,13 @@
+package lk.hemal.notly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotlyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotlyServerApplication.class, args);
+    }
+
+}
