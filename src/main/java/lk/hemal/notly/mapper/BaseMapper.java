27f84a376b54ec1d404c2b85b3dbefe40242d713
NipunHemal <nipunthemal@gmail.com>
2026-04-01 13:54:47 +0300

    add BaseMapper and UserMapper interfaces for entity-DTO mapping

---
+++ src/main/java/lk/hemal/notly/mapper/BaseMapper.java
@@ -0,0 +1,9 @@
+package lk.hemal.notly.mapper;
+
+import java.util.List;
+
+public interface BaseMapper<E, D> {
+    D toDto(E entity);
+    E toEntity(D dto);
+    List<D> toDtoList(List<E> entities);
+}
---
+++ src/main/java/lk/hemal/notly/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package lk.hemal.notly.mapper;
+
+import lk.hemal.notly.dto.response.UserResponseDto;
+import lk.hemal.notly.entity.User;
+
+public interface UserMapper extends BaseMapper<User, UserResponseDto>{
+}
