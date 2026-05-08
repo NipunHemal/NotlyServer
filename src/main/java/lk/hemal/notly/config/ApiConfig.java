56b9c7a9e310339313a90b4db3f06655cbe28a1b
NipunHemal <nipunthemal@gmail.com>
2026-05-08 12:40:07 +0530

    add ApiConfig class for centralized API configuration and CORS settings

---
+++ src/main/java/lk/hemal/notly/config/ApiConfig.java
@@ -0,0 +1,28 @@
+package lk.hemal.notly.config;
+
+import lombok.Getter;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
+public class ApiConfig {
+    public static final String API_VERSION = "/v1";
+    public static final String API_BASE_PATH = "/api" + API_VERSION;
+
+    // values will be set in application.yml or env
+    private final String[] allowedOrigins = {
+            "http://localhost:3000",
+            "http://127.0.0.1:3000"
+    };
+
+    private final boolean allowedCredentials = true;
+
+    private final String[] allowedMethods = {
+            "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"
+    };
+
+    private final String[] allowedHeaders = {
+            "Authorization", "Origin", "Content-Type", "Accept", "X-Requested-With"
+    };
+
+}
